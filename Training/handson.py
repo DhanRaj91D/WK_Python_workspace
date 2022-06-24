c1884a581e8e3f5b8ce044e40825c4259388b886
Dhanraj.Dhumal <dhanraj.sdhumal@gmail.com>
2022-06-24 16:59:22 +0530

    vowel count using dictionary

--- Training/handson.py
+++ Training/handson.py
@@ -12,4 +12,19 @@ for num in mylist:
     if num % 2 == 0:
         even_num.append(num)
 
+
+
 print(f"even no in list are {even_num}")
+
+
+# find no of occurance of vovels
+inputstr = input("Enter your value: ")
+vovel = "aeiou"
+
+vovel_count = {}.fromkeys(vovel,0)
+
+for char in inputstr:
+    if char in vovel:
+        vovel_count[char] += 1
+
+print(vovel_count)
